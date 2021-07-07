f5296038d31e795751d3eb80852c1930bacc6e66
Marcos Huanca <mhuanca93@gmail.com>
2021-07-07 09:53:35 -0300

    Añadido queSePuedaStockear

--- PBII20211QParcial2HuancaMarcos/src/ar/edu/unlam/pb2/Supermercado.java
+++ PBII20211QParcial2HuancaMarcos/src/ar/edu/unlam/pb2/Supermercado.java
@@ -17,18 +17,32 @@ public class Supermercado {
 	
 	public Supermercado(String nombre) {
 		// Constructor de la clase
+		this.nombre = nombre;
+		this.productosExistentes = new HashSet<>();
+		this.productosDisponibles = new ArrayList<>();
+		this.ventasRealizadas = new HashMap<Integer, Venta>();
 	}
 
 	public void ingresarProducto(Producto nuevo) {
-		// Ingresa un nuevo producto		
+		// Ingresa un nuevo producto
+		this.productosExistentes.add(nuevo);
+		this.productosDisponibles.add(nuevo);
 	}
 
 	public Set<Producto> getOfertaDeProductos() {
 		// Devuelve el conjunto de productos que se comercializan
+		return this.productosExistentes;
 	}
 	
 	public Integer getStock(Integer codigo) {
 		// Devuelve la cantidad de unidades de un producto determinado
+		int stock = 0;
+		for(Producto producto: this.productosDisponibles)
+		{
+			if(producto.getId()== codigo)
+				stock++;
+		}
+		return stock;
 	}
 	
 	private Boolean productoExiste(Integer codigoDeProducto) throws ProductoInexistente{
